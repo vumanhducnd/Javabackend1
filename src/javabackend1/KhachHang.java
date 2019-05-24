aba72ae4ad87f33fc329291518248abdb857d032
vuman_000 <you@example.com>
2019-05-24 12:28:42 +0700

    Them get set

--- src/javabackend1/KhachHang.java
+++ src/javabackend1/KhachHang.java
@@ -13,6 +13,31 @@ public class KhachHang {
   public  int ma;
   public  String ten;
   public  String diaChi;
+
+    public int getMa() {
+        return ma;
+    }
+
+    public void setMa(int ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
     
 }
+
 
